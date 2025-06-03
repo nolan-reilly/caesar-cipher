2f931a9d480c5df9090464702f910966b15488a8
Nolan Reilly <nolanreilly02@gmail.com>
2025-06-03 11:02:41 -0500

    Install & Run RuboCop

--- main.rb
+++ main.rb
@@ -1,23 +1,21 @@
-
-
 def caesar_cipher(string, shift)
-  alphabet = ("a".."z").to_a
-  capitalized_alphabet = alphabet.map { |element| element.upcase}
-  shift_num = shift % 26 
-  
+  alphabet = ('a'..'z').to_a
+  capitalized_alphabet = alphabet.map { |element| element.upcase }
+  shift_num = shift % 26
+
   cipher = [] # Store the ciphered array here
 
   # Loop through all the elements in the string
-  for c in string.chars
+  string.chars.each do |c|
     # Find out if the current character is in the alphabet and capitalized
-    is_capitalized = capitalized_alphabet.include?(c) ? true : false
+    is_capitalized = capitalized_alphabet.include?(c) || false
 
     # Shift characters based on shift_num and capitalize if it needs to be
     if alphabet.include?(c)
       index = alphabet.index(c)
 
       cipher.push(alphabet[(index + shift_num) % 26])
-    elsif is_capitalized and capitalized_alphabet.include?(c)
+    elsif is_capitalized && capitalized_alphabet.include?(c)
       index = capitalized_alphabet.index(c)
 
       cipher.push(capitalized_alphabet[(index + shift_num) % 26])
@@ -25,6 +23,6 @@ def caesar_cipher(string, shift)
       cipher.push(c)
     end
   end
-  
-  cipher.join()
+
+  cipher.join
 end
